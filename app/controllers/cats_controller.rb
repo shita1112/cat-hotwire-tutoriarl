f089087e48029c361d42424784692d7a4588bbd9
shita1112 <a1125ts@aiit.ac.jp>
2022-04-03 14:39:41 +0900

    編集のTurbo Streams化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -35,7 +35,6 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
