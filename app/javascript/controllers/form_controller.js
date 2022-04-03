80eba3678cadebc4505823641d5614ff69c01535
shita1112 <a1125ts@aiit.ac.jp>
2022-04-03 15:46:27 +0900

    インスタントサーチの実装（自前のdebounce）

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,6 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   submit() {
-    this.element.requestSubmit()
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 200)
   }
 }
