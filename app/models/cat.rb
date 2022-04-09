87451b2ceb7ee8a7ea480cfce2d3de396847c56d
shita1112 <a1125ts@aiit.ac.jp>
2022-04-09 13:20:22 +0900

    編集・登録・削除のコールバックにはbroadcasts_toを使うように修正

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,7 +2,5 @@ class Cat < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_create_commit -> { broadcast_prepend_to("cats") }
-  after_update_commit -> { broadcast_replace_to("cats") }
-  after_destroy_commit -> { broadcast_remove_to("cats") }
+  broadcasts_to -> (_cat) { "cats" }, inserts_by: :prepend
 end
